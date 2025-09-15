4c2cd0d2cb2b74678791ee44bf8016b2d2cda329
ANKIT YADAV <120283365+ankitamritlal@users.noreply.github.com>
2025-09-15 08:23:05 +0000

    conversion

---
+++ basic/01_convert.js
@@ -0,0 +1,15 @@
+// let score="33abc";
+// console.log(score+7);
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber+7);
+// console.log(typeof valueInNumber);
+let isloggedIn = 'ankit';
+let isLoggedIn1 = "";
+console.log(typeof isloggedIn);
+console.log(typeof isLoggedIn1);
+let booleanIsLoggedIn = Boolean(isloggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(isloggedIn  );
